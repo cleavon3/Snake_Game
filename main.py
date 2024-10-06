9d688217626cb04f0748830ddc83f637c4e894ea
cleavon <cleavon.design.com>
2024-10-06 07:37:32 +0100

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+import turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +11,7 @@ screen.title("my Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,7 +23,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    #detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
 
 screen.exitonclick()
